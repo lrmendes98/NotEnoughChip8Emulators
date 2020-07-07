573c1b70115616cad7fa095579dbda7a5ec1e7bd
luis <lrmmendes98@gmail.com>
2020-07-07 02:10:52 +0100

    changes to gitignore, added doxy comment example

--- code/include/app.hpp
+++ code/include/app.hpp
@@ -3,6 +3,16 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 
+/**
+ * A brief history of JavaDoc-style (C-style) comments.
+ *
+ * This is the typical JavaDoc-style C-style comment. It starts with two
+ * asterisks.
+ *
+ * @param theory Even if there is only one possible unified theory. it is just a
+ *               set of rules and equations.
+ */
+
 class App {
     public:
         App();
